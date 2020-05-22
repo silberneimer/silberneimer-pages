673dda98c0b365c6edf46fe965d4eafcd0e60449
Eimer <silbern.eimer@gmail.com>
2020-05-22 07:51:25 +0000

    wip

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {ScrollView, Text, View} from "react-native";
+import {ScrollView, Text, View, TextInput} from "react-native";
 import {
     heightPercentageToDP,
     listenOrientationChange,
@@ -7,9 +7,8 @@ import {
     widthPercentageToDP
 } from "react-native-responsive-screen";
 import Theme from "../../components/Theme";
-import Dialog from "../../components/assets/atoms/Dialog";
 import {BackgroundColor, FullStretch, Position} from "../../components/Layout";
-import { Box, AbsBox } from "../../components/assets/atoms/Box";
+import { Box, AbsBox, TapHighlightBox, TapOpacityBox } from "../../components/assets/atoms/Box";
 import FullScreenView from "../../components/templates/FullScreenView";
 
 export class Home extends Component<{}> {
@@ -25,28 +24,16 @@ export class Home extends Component<{}> {
     render() {
         // const isPhoneStyle = widthPercentageToDP(100) < 960;
         return (
-            <Box direction="bottom"
-              bg="green"
-              h={"100%"}
-              verticalAlign="space-around"
-              horizontalAlign="center">
-                <Box verticalAlign="space-around" horizontalAlign="center" w={50} h={50} bg="red">
+            <Box bg="green" h={"100%"}>
+                <Box alignCenter w={50} h={50} bg="red">
                     <Text>[A]</Text>
                 </Box>
-                <Box verticalAlign="space-around"
-              horizontalAlign="center" w={50} h={50} bg="blue">
+                <TapHighlightBox alignCenter w={50} h={50} bg="blue">
                     <Text>[B]</Text>
-                </Box>
-                <Box verticalAlign="space-around"
-                     horizontalAlign="center" w={50} h={50} bg="yellow">
+                </TapHighlightBox>
+                <TapOpacityBox alignCenter w={50} h={50} bg="yellow">
                     <Text>[C]</Text>
-                </Box>
-                <AbsBox w={50} h={50} bg="orange" verticalAlign="center"
-              horizontalAlign="center" style={{
-                    bottom: 0, left: 0
-                }}>
-                    <Text>[D]</Text>
-                </AbsBox>
+                </TapOpacityBox>
             </Box>
         )
     }
